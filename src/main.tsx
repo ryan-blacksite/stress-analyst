6539b85c229a2136c2a430b2f6d277505381f9e8
ryan-blacksite <ryan@blacksite.dev>
2026-04-11 15:33:50 -0500

    Fix workspace routing with hash-based navigation

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import Landing from './pages/Landing';
 import Workspace from './pages/Workspace';
 import './styles/global.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/workspace" element={<Workspace />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 );
